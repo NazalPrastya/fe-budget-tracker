13f99ab086dc38424531b21a0ec427629f240ca8
NazalPrastya <nazalprastya@gmail.com>
2025-06-01 12:45:32 +0700

    change baseurl

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL_PROD,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL_V1,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
